667eef63dabffb48d60a3b6dbef4d8accd9e71b6
Saiful Islam <saiful.i9489@gmail.com>
2025-12-14 02:28:59 -0500

    Added the check_processes function.

--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
-use std::process::{self, Command};
-use std::Error;
+use std::process::{self, Command, Child};
+use std::error::Error;
 
 use crate::config::{RestartPolicy, ServiceConfig};
 
@@ -10,7 +10,8 @@ pub struct ProcessManager {
 
 #[derive(Debug)]
 struct ProcessInfo {
-    pub child: std::process::Child,
+    pub child: Child,
+    pub config: ServiceConfig,
     restart_count: u32,
     last_exit_code: Option<i32>,
 }
@@ -24,7 +25,7 @@ impl ProcessManager {
 
     pub fn start_service(&mut self, config: &ServiceConfig) -> Result<i32, Box<dyn Error>> {
         let mut command = Command::new(&config.command);
-        command.args(&config.arg);
+        command.args(&config.args);
         command.envs(&config.env);
         let child = command.spawn()?;
         let pid = child.id() as i32;
@@ -33,6 +34,7 @@ impl ProcessManager {
             config.name.clone(),
             ProcessInfo {
                 child: child,
+                config: config.clone(),
                 restart_count: 0,
                 last_exit_code: None,
             },
@@ -41,5 +43,22 @@ impl ProcessManager {
         log::info!("Started service {} with PID {}", config.name, pid);
         
         Ok(pid)
+    }
+
+    pub fn check_processes(&mut self) -> Result<(), Box<dyn Error>> {
+        for (name, info) in self.processes.iter_mut() {
+            match info.child.try_wait()? {
+                // process exited
+                Some(status) => {
+                    log::warn!("Service {} exited with status {}", name, status);
+                    info.last_exit_code = status.code();
+
+                    // TODO: restart logic.
+                }
+                // process is still running
+                None => {}
+            }
+        }
+        ok(())
     }
 }
